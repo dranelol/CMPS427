39ed858b342c832ed62bcd92a5fd6bc0647a2c62
Ryan Durel <rdurel423@gmail.com>
2014-02-06 14:54:50 -0600

    Added Attributes
    Added attributes object which contains public properties for accessing
    stats, as well as methods for adding/subtracting attributes.

---
+++ Assets/Scripts/Entity/Attributes.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Container for entity attributes. Contains accessor methods for each attribute.
+/// If an entity needs more attributes than what this class provides, this class should be extended to include
+/// the extra attributes.
+/// </summary>
+public class Attributes
+{
+    public enum Stats
+    {
+        HEALTH,
+        RESOURCE,
+        POWER,
+        DEFENSE,
+        ATTACK_SPEED,
+        MOVEMENT_SPEED
+    };
+
+    private Dictionary<Stats, float> statList = new Dictionary<Stats,float>();
+
+    /// <summary>
+    /// Adds the required keys to the dictionary.
+    /// </summary>
+    public Attributes()
+    {
+        foreach (Stats stat in Enum.GetValues(typeof(Stats)))
+            statList.Add(stat, 0);
+    }
+
+    #region Health
+    public float Health
+    {
+        get
+        { return statList[Stats.HEALTH]; }
+        set { statList[Stats.HEALTH] = value; }
+    }
+    #endregion
+
+    #region Resource
+    public float Resource
+    {
+        get { return statList[Stats.RESOURCE]; }
+        set { statList[Stats.RESOURCE] = value; }
+    }
+    #endregion
+
+    #region Power
+    public float Power
+    {
+        get { return statList[Stats.POWER]; }
+        set { statList[Stats.POWER] = value; }
+    }
+    #endregion
+
+    #region Defense
+    public float Defense
+    {
+        get { return statList[Stats.DEFENSE]; }
+        set { statList[Stats.DEFENSE] = value; }
+    }
+    #endregion
+
+    #region Attack Speed
+    public float AttackSpeed
+    {
+        get { return statList[Stats.ATTACK_SPEED]; }
+        set { statList[Stats.ATTACK_SPEED] = value; }
+    }
+    #endregion
+
+    #region Movement Speed
+    public float MovementSpeed
+    {
+        get { return statList[Stats.MOVEMENT_SPEED]; }
+        set { statList[Stats.MOVEMENT_SPEED] = value; }
+    }
+
+    #endregion
+
+    #region Arithmetic Operations
+    /// <summary>
+    /// Adds attributes to the current attribute object.
+    /// </summary>
+    /// <param name="other">Object whose attributes will be added to the caller.</param>
+    public void Add(Attributes other)
+    {
+        Health += other.Health;
+        Resource += other.Resource;
+        Power += other.Power;
+        Defense += other.Defense;
+        AttackSpeed += other.AttackSpeed;
+        MovementSpeed += other.MovementSpeed;
+    }
+
+    /// <summary>
+    /// Subtracts attributes to the current attribute object.
+    /// </summary>
+    /// <param name="other">Object whose attributes will be subtracted from the caller.</param>
+    public void Subtract(Attributes other)
+    {
+        Health -= other.Health;
+        Resource -= other.Resource;
+        Power -= other.Power;
+        Defense -= other.Defense;
+        AttackSpeed -= other.AttackSpeed;
+        MovementSpeed -= other.MovementSpeed;
+    }
+    #endregion
+}
