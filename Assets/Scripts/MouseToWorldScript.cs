cf986c8c8d706ed50e855bb15ad6cb343cb9fe7c
ryanadair009 <ryan.adair009@gmail.com>
2014-02-07 11:28:54 -0600

    Cursor and Folders
    Added cursor projector and a bunch of folders. Please start placing
    assets in the correct folders and create new folders if needed.

---
+++ Assets/Scripts/MouseToWorldScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseToWorldScript : MonoBehaviour 
+{
+    private Vector3 _worldMousePosition;
+    public bool isActive = true;
+
+	void Start () 
+    {
+        _worldMousePosition = new Vector3(0, 0, 0);
+	}
+	
+	void Update () 
+    {
+        if (isActive)
+        {
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            Ray theRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float hitdist = 0;
+
+            if (playerPlane.Raycast(theRay, out hitdist))
+            {
+                transform.position = theRay.GetPoint(hitdist);
+            }
+        }
+	}
+
+    public Vector3 WorldMousePosition
+    {
+        get { return _worldMousePosition; }
+    }
+}
