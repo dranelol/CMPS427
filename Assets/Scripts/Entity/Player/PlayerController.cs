054d0c59a6d92f2aad10378407f8e151c8262632
Ryan Durel <rdurel423@gmail.com>
2014-02-13 13:55:25 -0600

    Added Player Controller
    Added controller class for handling player input. Currently it's behaves
    about the same as NavMesh.

---
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Move/Attack") != 0) 
+        {
+            Plane player = new Plane(Vector3.up, transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float hitDistance = 0;
+            // TODO: Check for ray hit on enemy.
+            if (player.Raycast(ray, out hitDistance))
+            {
+                GetComponent<MovementFSM>().SetPath(ray.GetPoint(hitDistance));
+            }
+        }
+	}
+}
