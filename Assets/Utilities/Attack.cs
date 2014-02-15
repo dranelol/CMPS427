1bee42b7ed5c063cbefb49e314ccec3971264387
dranelol <cardsfan853@gmail.com>
2014-02-14 18:21:17 -0600

    still trying to fix null reference bug

--- Assets/Utilities/Attack.cs
+++ Assets/Utilities/Attack.cs
@@ -10,25 +10,37 @@ public class Attack
 
         Vector3 forward = attacker.forward.normalized;
 
-        Collider[] colliders = Physics.OverlapSphere(attacker.position, attackRange);
+        int enemyMask = LayerMask.NameToLayer("Enemy");
+        int playerMask = LayerMask.NameToLayer("Player");
+
+        Collider[] colliders = Physics.OverlapSphere(attacker.position, attackRange, 1 << enemyMask);
 
         foreach (Collider collider in colliders)
         {
-            Vector3 enemyVector = (collider.transform.position - attacker.position).normalized;
-
+            //Vector3 enemyVector = collider.transform.position - attacker.position;
+            Vector3 enemyVector = collider.transform.position - attacker.position;
+            //Debug.Log(enemyVector);
+            //Debug.Log(Vector3.Angle(forward, enemyVector));
+            
             if (Vector3.Angle(forward, enemyVector) < attackAngle)
             {
-                Debug.Log(Vector3.Angle(forward, enemyVector).ToString());
+                Debug.Log(collider.ToString());
+                //Debug.Log(Vector3.Angle(forward, enemyVector));
+                //Debug.Log(Vector3.Angle(forward, enemyVector).ToString());
                 // draw ray between enemy and player
                 // raycast with a layermask for enemies
+                //Debug.Log("enemy in angle: " + Vector3.Angle(forward, enemyVector).ToString());
                 RaycastHit hit;
-                int enemyMask = LayerMask.NameToLayer("Enemy");
-                Physics.Raycast(new Ray(collider.transform.position, enemyVector),out hit, attackRange, enemyMask);
-
+                Physics.Raycast(new Ray(collider.transform.position, enemyVector),out hit, attackRange, 1 << playerMask);
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    Debug.Log("asd");
+                }
                 // if the first thing the raycast hits is the player, player do damage to enemy
 
-                Debug.Log(hit.ToString());
-                
+                //Debug.Log(hit.ToString());
+                //Debug.Log(hit.collider.tag);
+                //Debug.Log(hit.collider.gameObject.tag);
                 /*
                 if (hit.collider.gameObject.tag == "Player")
                 {
