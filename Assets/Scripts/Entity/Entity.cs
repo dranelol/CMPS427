4bf936b3dc70dc437e24328d3c43b8bee59c5db0
Davis725 <d.davis6835@gmail.com>
2014-02-18 13:35:18 -0600

    Merge

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,24 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class Entity : MonoBehaviour
+public interface Entity
 {
-    public enum EquipmentSlots
-    {
-        HEAD,
-        TORSO,
-        LEFT_ARM,
-        RIGHT_ARM,
-        FEET
-    };
+    /* Buff List - list of timed attribute changes. Used to remove buffs after a given time (I dunno how to do dat)
+     * Current Attribute object
+     * Temp Attribute object - contains all affects entity is currently under (Current - Temp = Base Attributes) */
 
-    public float currentHP; // Currently unused.
-    public Attributes currentAtt; // The entity's current total attributes
-    public Attributes equipAtt; // Attribute changes that are added on from equipment stat changes
-    public Attributes buffAtt; // Attribute changes that are added on from buffs/debuffs
-
-    private Dictionary<EquipmentSlots, Attributes> equipmentStats = new Dictionary<EquipmentSlots, Attributes>();
+    ArrayList bufflist;
+    Attributes currentAtt;
+    Attributes tempAtt;
 
     /// <summary>
     /// Creates the entity with a given set of base attributes,
@@ -27,66 +18,32 @@ public class Entity : MonoBehaviour
     public Entity(Attributes att)
     {
         currentAtt = att;
-        equipAtt = new Attributes();
+        tempAtt = new Attributes();
+        bufflist = new ArrayList();
     }
 
     /// <summary>
-    /// Add the attribute changes of an item to the entity. The item must correlate to one of the equipment slots,
-    /// HEAD, TORSO, LEFT_ARM, RIGHT_ARM, or FEET. Attribute changes are taken as an attributes object. Returns
-    /// false if the slot is already filled.
+    /// Add the attributes of an equipment item to the character.
     /// </summary>
-    /// <param name="slot">The equipment slot being filled.</param>
-    /// <param name="itemAtt">The attributes of the item being equipped.</param>
-    /// <returns></returns>
-    public bool addEquipment(EquipmentSlots slot, Attributes itemAtt)
+    /// <param name="equ">The attributes object from an item being equipped.</param>
+    public void addEquipmentAtt(Attributes equ)
     {
-        if (equipmentStats.ContainsKey(slot))
-            return false;
-        else
-        {
-            equipmentStats.Add(slot, itemAtt);
-            currentAtt.Add(itemAtt);
-            equipAtt.Add(itemAtt);
-            return true;
-        }
+        currentAtt.Add(equ);
+        tempAtt.Add(equ);
     }
 
     /// <summary>
-    /// Remove an item from an equipment slot, clearing that slot. Returns false if that slot is already empty.
+    /// Removing the attributes of an equipment item from the character.
     /// </summary>
-    /// <param name="slot"></param>
-    /// <returns></returns>
-    public bool removeEquipment(EquipmentSlots slot)
+    /// <param name="equ">The attribute object from the item being unequipped.</param>
+    public void removeEquipmentAtt(Attributes equ)
     {
-        if (equipmentStats.ContainsKey(slot))
-        {
-            Attributes removed = equipmentStats[slot];
-            equipmentStats.Remove(slot);
-            currentAtt.Subtract(removed);
-            equipAtt.Subtract(removed);
-            return true;
-        }
-        else
-            return false;
+        currentAtt.Subtract(equ);
+        tempAtt.Subtract(equ);
     }
 
-    /// <summary>
-    /// Adds a buff to the entity. The buff is given by an attributes object that is added to the entity's current
-    /// stats. The duration of the buff is given in seconds.
-    /// </summary>
-    /// <param name="statChange">The attributes to be added by this buff</param>
-    /// <param name="duration">The duration of this buff in seconds</param>
-    public void addBuff(Attributes statChange, float duration)
+    public void addBuff(Attributes equ, float duration)
     {
-        StartCoroutine(newbuff(statChange, duration));
-    }
 
-    private IEnumerator newbuff(Attributes s, float d)
-    {
-        currentAtt.Add(s);
-        buffAtt.Add(s);
-        yield return new WaitForSeconds(d);
-        currentAtt.Subtract(s);
-        buffAtt.Subtract(s);
     }
 }
