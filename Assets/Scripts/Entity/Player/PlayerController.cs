21f6e96a3e78292bf7c847d3db3d5f7b0928b9d6
dranelol <cardsfan853@gmail.com>
2014-02-18 16:57:52 -0600

    resolved RyanD's player controller
    got diddling working, got player controller working,

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
     // Range at which the player stops moving and begins attacking. BAM
@@ -8,14 +9,44 @@ public class PlayerController : MonoBehaviour {
 	// Need to keep track of enemy if we click to attack it.
 	private Vector3 targetPosition;
 
+    public NavMeshAgent agent;
+
+    private bool hadouken = false;
+
 	// Use this for initialization
 	void Start () {
 		targetPosition = Vector3.zero;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.DrawRay(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, Rotations.RotateAboutY(new Vector3(transform.forward.x * 5.0f, transform.forward.y, transform.forward.z * 5.0f), -22.5f));
+        Debug.DrawRay(transform.position, Rotations.RotateAboutY(new Vector3(transform.forward.x * 5.0f, transform.forward.y, transform.forward.z * 5.0f), 22.5f));
+        
+        // if our agent actually has a path to move to
+        if (agent.hasPath == true)
+        {
+            // find the next steering target and his current position, without caring about y-axis
+            Vector3 steeringTarget = new Vector3(agent.steeringTarget.x, 0, agent.steeringTarget.z);
+            Vector3 playerPosition = new Vector3(transform.position.x, 0, transform.position.z);
+
+            // create a quaternion to rotate towards the next target
+            Quaternion quat = Quaternion.LookRotation(steeringTarget - playerPosition);
+
+            //apply quaternion to the player's rotation
+            transform.rotation = quat;
+        }
+        
+        if (Vector3.Distance(transform.position, agent.destination) < 1.0f)
+        {
+            agent.ResetPath();
+        }
         // If we have a target...
+
         if (targetPosition != Vector3.zero)
         {
             // If we're in attack range...
@@ -50,11 +81,56 @@ public class PlayerController : MonoBehaviour {
 					targetPosition = target.collider.gameObject.transform.position;
 				else
 				{
+                    
                     // Otherwise, move towards the point of collision.
 					targetPosition = Vector3.zero;
 					GetComponent<MovementFSM>().SetPath(target.point);
+
 				}
 			}
+
         }
+
+        // HADOUKEN
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (hadouken == false)
+            {
+                agent.radius = 10f;
+                hadouken = true;
+            }
+
+            else
+            {
+                agent.radius = 0.5f;
+                hadouken = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("lelele");
+            List<GameObject> attacked = Attack.OnAttack(transform, 45f, 5f);
+            foreach (GameObject enemy in attacked)
+            {
+                Debug.Log(enemy.GetInstanceID().ToString());
+                //Color enemyColor = enemy.renderer.material.color;
+                //enemy.renderer.material.color = ;
+                //Color enemyColor = enemy.renderer.material.GetColor("_Color");
+                //Color enemyColorTint = enemy.renderer.material.GetColor("_TintColor");
+                //float intensity = enemyColorTint.a + 50;
+                //Debug.Log(enemyColor.ToString());
+                //enemyColor.r -= (byte)0.5;
+                //enemyColor.b -= (byte)0.5;
+                //enemyColor.g -= (byte)0.5;
+
+                //Color tintColor = new Color32(255, 23, 0, (byte)intensity);
+                //enemy.renderer.material.SetColor("_TintColor", tintColor);
+                //enemy.renderer.material.SetColor("_Color", enemyColor);
+                //enemy.renderer.material.SetColor("_Color", new Color(enemyColor.r - (byte)1, enemyColor.g - (byte)1, enemyColor.b - (byte)1, enemyColor.a));
+                //enemy.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+                enemy.renderer.material.SetColor("_Color", Color.red);
+            }
+        }
+
 	}
 }
