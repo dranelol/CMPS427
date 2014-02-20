160c6636f188dc32846a52e8622c9f9132c5b9a2
jpd5367 <lilgsrsoldier@gmail.com>
2014-02-20 17:45:20 -0600

    more rebasing... almost done

---
+++ Assets/Scripts/equipmentFactory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class equipmentFactory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public equipment buildEquipment() {
+
+        equipment tempEquipment = new equipment();
+
+        
+
+        return tempEquipment;
+
+
+    }
+
+
+}
