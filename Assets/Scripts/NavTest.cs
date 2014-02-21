85e869240c9b2c397b818ff433ab43b4f15506b4
jpd5367 <lilgsrsoldier@gmail.com>
2014-02-20 18:02:30 -0600

    adding rotation control

--- Assets/Scripts/NavTest.cs
+++ Assets/Scripts/NavTest.cs
@@ -14,17 +14,19 @@ public class NavTest : MonoBehaviour
     public MovementFSM MoveFSM;
     public NavMeshAgent agent;
     public CharacterController controller;
+	public float RotationSpeed = 10f;
 
     void Start()
     {
         MoveFSM = GetComponent<MovementFSM>();
         agent = GetComponent<NavMeshAgent>();
         controller = GetComponent<CharacterController>();
+		agent.acceleration = 1000f;
         agent.updateRotation = false;
         
     }
 
-	void Update () 
+	void FixedUpdate () 
     {
         Debug.DrawRay(transform.position, transform.forward);
         Debug.DrawRay(transform.position, Rotations.RotateAboutY(new Vector3(transform.forward.x * 5.0f, transform.forward.y, transform.forward.z * 5.0f), -22.5f));
@@ -33,15 +35,28 @@ public class NavTest : MonoBehaviour
         //Debug.Log(transform.forward);
         //Debug.Log(new Vector3(transform.forward.x * 5.0f, transform.forward.y, transform.forward.z * 5.0f));
         
-        if (agent.velocity != Vector3.zero)
+        if (agent.hasPath)
         {
             Vector3 newVector = (transform.position + agent.velocity.normalized);
             Vector3 target = newVector - transform.position;
             
-            Quaternion quat = Quaternion.LookRotation(target);
-            transform.rotation = quat;
+            
+		//	Quaternion quat = Quaternion.LookRotation(target);
+         //  transform.rotation = quat;
+
+			Vector3 tempRotation = transform.rotation.eulerAngles ;
+			tempRotation.y = Mathf.LerpAngle(transform.rotation.eulerAngles.y,  Quaternion.LookRotation(target).eulerAngles.y,Time.deltaTime * RotationSpeed);
+			transform.rotation = Quaternion.Euler(tempRotation);
+		
+
+
         }
+<<<<<<< HEAD
         
+=======
+
+
+>>>>>>> rotation controll added for player
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 NavTarget;
