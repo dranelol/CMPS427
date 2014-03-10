9a718975c4d8d09266efabd5672981a2105d573b
dranelol <cardsfan853@gmail.com>
2014-03-10 11:36:59 -0500

    AUTO ATTACK LOL

--- Assets/Scripts/Entity/Player/PlayerEntity.cs
+++ Assets/Scripts/Entity/Player/PlayerEntity.cs
@@ -26,6 +26,9 @@ public class PlayerEntity : Entity {
 	// Update is called once per frame
 	public void Update () 
     {
+        abilities[4].AttackHandler(GameObject.FindGameObjectWithTag("Player"), true);
+        abilities[5].AttackHandler(GameObject.FindGameObjectWithTag("Player"), true);
+
         // update these on-demand instead of every update
 
         //power = currentAtt.Power;
