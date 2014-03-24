914d18f07bfbfb92d75f9803f81ab5c7d4b6b903
jpd5367 <lilgsrsoldier@gmail.com>
2014-03-24 15:47:16 -0500

    Ability cooldown GUI
    Ability cooldown GUI

--- Assets/Scripts/HUD_GUI.cs
+++ Assets/Scripts/HUD_GUI.cs
@@ -12,6 +12,13 @@ public class HUD_GUI : MonoBehaviour {
 	public Texture Ability3;
 	public Texture Ability4;
 	public Texture Ability5;
+	public Texture AbilityCoolDown;
+
+	public float Ability1CoolDownTime = .5f;
+	public float Ability2CoolDownTime = .5f;
+	public float Ability3CoolDownTime = .5f;
+	public float Ability4CoolDownTime = .5f;
+	public float Ability5CoolDownTime = .5f;
 
 	public GUIStyle AbiltyStyle;
 
@@ -34,6 +41,7 @@ public class HUD_GUI : MonoBehaviour {
 	public Vector2 HealthLiquidPos;
 	public Vector2 HealthOverlaySize;
 	public Vector2 HealthLiquidSize;
+
 
 	public float native_width;
 	public float native_height;
@@ -89,13 +97,73 @@ public class HUD_GUI : MonoBehaviour {
 		if(health >1)health = 1f;
 
 		GUI.BeginGroup(AbilitiesOuterBox);{//161.9 436, 134.2,48.3
-			GUI.Button(AbilityOneBox,Ability1,AbiltyStyle);//6.44,.03,23.8,43.7
-			GUI.Button(AbilityTwoBox,Ability2,AbiltyStyle);//29.53,2.2,24.5,42.5
-			GUI.Button(AbilityThreeBox,Ability3,AbiltyStyle);//56.4,0,23.3,43.7
-			GUI.Button(AbilityFourBox,Ability4,AbiltyStyle);//81.4,0,24.32,43.7
-			GUI.Button(AbilityFiveBox,Ability5,AbiltyStyle);//106,0,26.4,44
+			GUI.DrawTexture(AbilityOneBox,Ability1);//6.44,.03,23.8,43.7
+
+			GUI.DrawTextureWithTexCoords(new Rect(AbilityOneBox.x,//0,
+			                                      AbilityOneBox.y + (AbilityOneBox.height * (1f-Ability1CoolDownTime)),//100-(100*health),// this sinks images clipping the bottom
+			                                      AbilityOneBox.width,//100f,
+			                                      AbilityOneBox.height),//100f),
+			                             AbilityCoolDown, // texture
+			                             new Rect(0f,
+			         Ability1CoolDownTime, // this adjust image to keep stationary
+			         1f,
+			         1f
+			         ));
+
+			GUI.DrawTexture(AbilityTwoBox,Ability2);//29.53,2.2,24.5,42.5
+
+			GUI.DrawTextureWithTexCoords(new Rect(AbilityTwoBox.x,//0,
+			                                      AbilityTwoBox.y + (AbilityTwoBox.height * (1f-Ability2CoolDownTime)),//100-(100*health),// this sinks images clipping the bottom
+			                                      AbilityTwoBox.width,//100f,
+			                                      AbilityTwoBox.height),//100f),
+			                             AbilityCoolDown, // texture
+			                             new Rect(0f,
+			         Ability2CoolDownTime, // this adjust image to keep stationary
+			         1f,
+			         1f
+			         ));
+
+			GUI.DrawTexture(AbilityThreeBox,Ability3);//56.4,0,23.3,43.7
+
+			GUI.DrawTextureWithTexCoords(new Rect(AbilityThreeBox.x,//0,
+			                                      AbilityThreeBox.y + (AbilityThreeBox.height * (1f-Ability3CoolDownTime)),//100-(100*health),// this sinks images clipping the bottom
+			                                      AbilityThreeBox.width,//100f,
+			                                      AbilityThreeBox.height),//100f),
+			                             AbilityCoolDown, // texture
+			                             new Rect(0f,
+			         Ability3CoolDownTime, // this adjust image to keep stationary
+			         1f,
+			         1f
+			         ));
+
+
+			GUI.DrawTexture(AbilityFourBox,Ability4);//81.4,0,24.32,43.7
+
+			GUI.DrawTextureWithTexCoords(new Rect(AbilityFourBox.x,//0,
+			                                      AbilityFourBox.y + (AbilityFourBox.height * (1f-Ability4CoolDownTime)),//100-(100*health),// this sinks images clipping the bottom
+			                                      AbilityFourBox.width,//100f,
+			                                      AbilityFourBox.height),//100f),
+			                             AbilityCoolDown, // texture
+			                             new Rect(0f,
+			         Ability4CoolDownTime, // this adjust image to keep stationary
+			         1f,
+			         1f
+			         ));
+
+
+			GUI.DrawTexture(AbilityFiveBox,Ability5);//106,0,26.4,44
 			
-			
+			GUI.DrawTextureWithTexCoords(new Rect(AbilityFiveBox.x,//0,
+			                                      AbilityFiveBox.y + (AbilityFiveBox.height * (1f-Ability5CoolDownTime)),//100-(100*health),// this sinks images clipping the bottom
+			                                      AbilityFiveBox.width,//100f,
+			                                      AbilityFiveBox.height),//100f),
+			                             AbilityCoolDown, // texture
+			                             new Rect(0f,
+			         Ability5CoolDownTime, // this adjust image to keep stationary
+			         1f,
+			         1f
+			         ));
+
 			
 			
 		}GUI.EndGroup();
