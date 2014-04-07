e0fdccaa534b9dbfb212a86471b51dff965cfd63
dranelol <cardsfan853@gmail.com>
2014-04-07 00:42:02 -0500

    new particles, orbit rotations

--- Assets/Scripts/Effects/OrbRotate.cs
+++ Assets/Scripts/Effects/OrbRotate.cs
@@ -7,6 +7,8 @@ public class OrbRotate : MonoBehaviour
     public float maxHeight;
     public float angularSpeed;
     public float oscillationSpeed;
+    public string rotationAxis;
+    public float orbitScale;
 
     void Awake()
     {
@@ -16,13 +18,55 @@ public class OrbRotate : MonoBehaviour
 	void Update () 
     {
         //newPosition.y = Mathf.Lerp(minHeight, maxHeight, Time.time);
+        if (rotationAxis == "x" || rotationAxis == "X")
+        {
+            transform.RotateAround(transform.parent.transform.position, Vector3.left, angularSpeed * Time.deltaTime);
 
-        transform.RotateAround(transform.parent.transform.position, Vector3.up, angularSpeed * Time.deltaTime);
-        Vector3 newPosition = transform.position;
+            Vector3 newPosition = transform.position;
 
-        //newPosition.y = MathHelper.Sinerp(minHeight, maxHeight, Time.time);
-        newPosition.y = Mathf.Sin(Time.time * oscillationSpeed) * maxHeight + maxHeight;
+            //newPosition.x = MathHelper.Sinerp(minHeight, maxHeight, Time.time);
+            if (oscillationSpeed > 0.0f)
+            {
+                newPosition.x = Mathf.Sin(Time.time * oscillationSpeed) * maxHeight + maxHeight;
+            }
 
-        transform.position = newPosition;
+            transform.position = newPosition;
+        }
+
+        else if (rotationAxis == "y" || rotationAxis == "Y")
+        {
+            transform.RotateAround(transform.parent.transform.position, Vector3.up, angularSpeed * Time.deltaTime);
+
+            Vector3 newPosition = transform.position;
+
+            //newPosition.y = MathHelper.Sinerp(minHeight, maxHeight, Time.time);
+            if (oscillationSpeed > 0.0f)
+            {
+                newPosition.y = Mathf.Sin(Time.time * oscillationSpeed) * maxHeight + maxHeight;
+            }
+
+            transform.position = newPosition;
+        }
+
+        else
+        {
+            transform.RotateAround(transform.parent.transform.position, Vector3.forward, angularSpeed * Time.deltaTime);
+
+            
+
+            //newPosition.z = MathHelper.Sinerp(minHeight, maxHeight, Time.time);
+            if (oscillationSpeed > 0.0f)
+            {
+                Vector3 newPosition = transform.position;
+                newPosition.z = Mathf.Sin(Time.time * oscillationSpeed) * maxHeight + maxHeight;
+                transform.position = newPosition;
+            }
+
+            
+
+           
+        }
+
+        
 	}
 }
--- Assets/Scripts/Effects/OrbSpawn.cs
+++ Assets/Scripts/Effects/OrbSpawn.cs
@@ -9,16 +9,46 @@ public class OrbSpawn : MonoBehaviour
     public float maxHeight;
     public float angularSpeed;
     public float oscillationSpeed;
+    public string rotationAxis;
+    public float orbitScale;
 
 	void Awake () 
     {
         for (int i = 0; i < orbAmount; i++)
         {
-            GameObject newOrb = (GameObject)GameObject.Instantiate(orb, transform.parent.transform.forward + Rotations.RotateAboutY(transform.parent.transform.forward, (360 / orbAmount) * i) * 2, Quaternion.identity);
+            Vector3 newOrbPos = Vector3.zero;
+
+            if (rotationAxis == "x" || rotationAxis == "X")
+            {
+                newOrbPos = Rotations.RotateAboutX(transform.parent.transform.forward * orbitScale, (360 / orbAmount) * i) * 2;
+                newOrbPos.x = minHeight;
+            }
+
+            else if (rotationAxis == "y" || rotationAxis == "Y")
+            {
+                newOrbPos = Rotations.RotateAboutY(transform.parent.transform.forward * orbitScale, (360 / orbAmount) * i) * 2;
+                newOrbPos.y = minHeight;
+            }
+
+            else
+            {
+                newOrbPos = Rotations.RotateAboutZ(transform.parent.transform.forward * orbitScale, (360 / orbAmount) * i) * 2;
+                //newOrbPos.z = minHeight;
+
+                Debug.Log((transform.parent.transform.forward * orbitScale).ToString());
+                Debug.Log(newOrbPos);
+            }
+
+            //newOrbPos = Rotations.RotateAboutY(transform.parent.transform.forward, (360 / orbAmount) * i) * 2;
+            //newOrbPos.y = minHeight;
+
+            GameObject newOrb = (GameObject)GameObject.Instantiate(orb, newOrbPos, transform.rotation);
             newOrb.GetComponent<OrbRotate>().minHeight = minHeight;
             newOrb.GetComponent<OrbRotate>().maxHeight = maxHeight;
             newOrb.GetComponent<OrbRotate>().angularSpeed = angularSpeed;
             newOrb.GetComponent<OrbRotate>().oscillationSpeed = oscillationSpeed;
+            newOrb.GetComponent<OrbRotate>().rotationAxis = rotationAxis;
+            newOrb.GetComponent<OrbRotate>().orbitScale = orbitScale;
             newOrb.transform.parent = transform;
             
         }
