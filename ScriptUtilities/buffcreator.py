65e6969ec74ae28d1d31292f4fc14d89f494b798
sercril <august.montalbano@gmail.com>
2014-04-08 14:01:09 -0500

    buff creator takes files; expanding for more values

--- ScriptUtilities/buffcreator.py
+++ ScriptUtilities/buffcreator.py
@@ -2,38 +2,88 @@ from argparse import *
 
 
 
-
-
+class BuffException(Exception):
+	pass
 
 buffTypes = ["Buff", "Debuff"]
 modules = ["HoT", "DoT", "Invigorate", "Exhaust", "FortifyAttribute", "DamageAttribute"]
+modtypeStats = ["Percentage","Value"]
+damageTypeStats = ["AIR", "EARTH", "FIRE", "NONE", "PHYSICAL", "SHADOW", "WATER"]
+attStats = ["ATTACK_SPEED", "DEFENSE", "HEALTH", "MAX_DAMAGE", "MIN_DAMAGE", "MOVEMENT_SPEED", "POWER", "RESOURCE"]
 
-parser = ArgumentParser(description='Generates Buffs')
-parser.add_argument('-f', action="store", help="File containing a comma separated list of buffs.")
+parser = ArgumentParser(description="Generates Buffs")
+parser.add_argument('inputFile', action="store", help="File containing a | separated list of buffs.")
+
+newBuff = parser.parse_args()
+
+
+template = open("template_file", "r")
+tempText = template.read()
+template.close()
+
+with open(newBuff.inputFile) as f:
+	for line in f:
+		inputList = line.split("|")
+		iCount = 0
+		for i in inputList:
+			inputList[iCount] = i.strip()
+			iCount+=1
+
+
+		if inputList[4] is not in modules:
+			raise BuffException("The module given for " +inputList[0]+ " does not exist!")
+		elif " " is in inputList[0]:
+			raise BuffException("The class name given for " +inputList[0]+ " must not contain spaces.")
+		elif inputList[3] is not in buffTypes:
+			raise BuffException("The buff type given for "+inputList[0]+" is not valid. Must be Buff or Debuff")
+
+
+		
+
+		if inputList[4] == "HoT":
+			actualBuff = inputList[4]+"(ModType."+inputList[5]+","+inputList[6]+"f)"
+		elif inputList[4] == "DoT":
+			actualBuff = inputList[4]+"(DamageType."+inputList[5]+","+inputList[6]+"f)"
+		elif inputList[4] == "Invigorate":
+			actualBuff = inputList[4]+"("+inputList[6]+"f)"
+		elif inputList[4] == "Exhaust":
+			actualBuff = inputList[4]+"("+inputList[6]+"f)"
+		elif inputList[4] == "FortifyAttribute":
+			actualBuff = inputList[4]+"(Attributes.Stats."+inputList[5]+","+inputList[6]+"f)"
+		elif inputList[4] == "DamageAttribute":
+			actualBuff = inputList[4]+"(Attributes.Stats."+inputList[5]+","+inputList[6]+"f)"
+		
+
+
+
+
+		buffType = "AuraType."+inputList[3]
+
+
+		modText = tempText.replace("BUFF_CLASS_NAME", inputList[0], 5).replace("BUFF_NAME", inputList[1], 5).replace("THE_ACTUAL_BUFF", actualBuff, 5).replace("BUFF_TYPE", buffType, 5).replace("BUFF_FLAVOR_TEXT", inputList[2], 5)
+
+		newBuffFile = open(inputList[0]+".cs", "w")
+
+		newBuffFile.write(modText)
+
+		
+		newBuffFile.close()
+
+
+
 
 '''
+Old Single Buff Creation Code
+
 parser.add_argument('cName', action='store', help="Name of the class for the buff (cannot contain spaces)", required=False)
 parser.add_argument('bName', action='store', help="Name of the buff (can contain spaces)", required=False)
 parser.add_argument('bType', action='store', choices=buffTypes, help="Specifies Buff or Debuff", required=False)
 parser.add_argument('mod', action='store', choices=modules, help="Module for the buff to use.", required=False)
 parser.add_argument('effectThing', action='store',  help="Thing the buff will affect", required=False)
 parser.add_argument('magnitude', action='store', help="Magnitude of the effect", required=False)
-'''
-newBuff = parser.parse_args()
-
-print(newBuff)
-
-raise SystemExit
-
 template = open("template_file", "r")
 
-
-
 tempText = template.read()
-
-
-
-
 
 if newBuff.mod == "HoT":
 	actualBuff = newBuff.mod+"(newBuff.modType."+newBuff.effectThing+","+newBuff.magnitude+"f)"
@@ -62,3 +112,4 @@ newBuffFile.write(modText)
 
 template.close()
 newBuffFile.close()
+'''
