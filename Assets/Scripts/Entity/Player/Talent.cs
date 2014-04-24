0aa40226b88d60ca3e557351e0e5e64dc2c08207
sercril <august.montalbano@gmail.com>
2014-04-23 20:21:48 -0500

    Talent Backend without ability adding

---
+++ Assets/Scripts/Entity/Player/Talent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Talent
+{
+
+    private int currentPoints; //Current points spent on the talent
+
+    public int CurrentPoints
+    {
+        get { return currentPoints; }
+
+        set { currentPoints = value; }
+    }
+
+    private int maxPoints; //Max number of points the talent will have.
+    public int MaxPoints
+    {
+        get { return MaxPoints; }
+    }
+
+    private int depth;
+    public int Depth
+    {
+        get { return depth; }
+    }
+
+    private Ability ability; //The ability spending points on this talent will unlock.
+    
+
+    /// <summary>
+    /// Talent Constructor
+    /// </summary>
+    /// <param name="maxPoints">Max number of points the talent will have.</param>
+    /// <param name="ability">The ability spending points on this talent will unlock.</param>
+    /// <param name="depth">Depth the talent is on the tree.</param>
+    public Talent(int maxPoints, Ability ability, int depth)
+    {
+        currentPoints = 0;
+        this.maxPoints = maxPoints;
+        this.ability = ability;
+        this.depth = depth;
+    }
+
+    public void AddAblilty()
+    {
+        //Add ability to spellbook
+
+    }
+
+    public void RemoveAblilty()
+    {
+        //Remove ability from spellbook
+
+    }
+}
---
+++ Assets/Scripts/Entity/Player/TalentManager.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TalentManager : MonoBehaviour
+{
+    public const int talentPointIncrement = 1; //Number of talent points given
+    public const int depthMultiplier = 5; //Mulitple of points needed to advance to the next tier of talents
+
+    private int totalTalentPoints; //Count of all talent points earned.
+
+    private int talentPointPool; //Count of unused talent points
+    public int TalentPointPool
+    {
+        get { return talentPointPool; }
+    }
+
+    private int mightTreePoints;
+    public int MightTreePoints
+    {
+        get { return mightTreePoints; }
+    }
+
+    private int magicTreePoints;
+    public int MagicTreePoints
+    {
+        get { return magicTreePoints; }
+    }
+
+    private HashSet<Talent> mightTree;
+    private HashSet<Talent> magicTree;
+
+    private PlayerController playerController;
+    
+    void Awake()
+    {
+        playerController = transform.GetComponent<PlayerController>();
+
+        mightTree = new HashSet<Talent>();
+        magicTree = new HashSet<Talent>();
+
+        mightTreePoints = 0;
+        magicTreePoints = 0;
+    }
+    
+    
+    // Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void SpendPoint(Talent talent)
+    {
+
+        if (talentPointPool > 0 && talent.CurrentPoints < talent.MaxPoints && IsTalentActive(talent) == true)
+        {
+            talentPointPool--;
+            talent.CurrentPoints++;
+
+            if (mightTree.Contains(talent) == true)
+            {
+                mightTreePoints++;
+            }
+            else if (magicTree.Contains(talent) == true)
+            {
+                magicTreePoints++;
+            }
+        }
+    }
+
+    public void RemovePoint(Talent talent)
+    {
+
+        if (talent.CurrentPoints > 0)
+        {
+
+            if (mightTree.Contains(talent) == true)
+            {
+
+                //Checks to see that there are no talents with points in them at a lower depth in the tree than the current talent.
+                foreach (Talent t in mightTree)
+                {
+                    if (t.Depth > talent.Depth && t.CurrentPoints > 0)
+                    {
+                        return;
+                    }
+                }
+
+                mightTreePoints--;
+            }
+            else if (magicTree.Contains(talent) == true)
+            {
+
+                //Checks to see that there are no talents with points in them at a lower depth in the tree than the current talent.
+                foreach (Talent t in magicTree)
+                {
+                    if (t.Depth > talent.Depth && t.CurrentPoints > 0)
+                    {
+                        return;
+                    }
+                }
+                
+                magicTreePoints--;
+            }
+
+            talentPointPool++;
+            talent.CurrentPoints--;
+        }
+    }
+
+    public void Respec()
+    {
+        talentPointPool = totalTalentPoints;
+
+        foreach (Talent t in mightTree)
+        {
+            t.CurrentPoints = 0;
+            t.RemoveAblilty();
+        }
+
+        foreach (Talent t in magicTree)
+        {
+            t.CurrentPoints = 0;
+            t.RemoveAblilty();
+        }
+    }
+
+    public bool IsTalentActive(Talent talent)
+    {
+        if (mightTree.Contains(talent) == true)
+        {
+            if (mightTreePoints >= talent.Depth * depthMultiplier)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (magicTree.Contains(talent) == true)
+        {
+            if (magicTreePoints >= talent.Depth * depthMultiplier)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
