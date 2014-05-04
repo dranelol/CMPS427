37b4e2e041ec2a13f2afa436c9dddebb62bc819c
dranelol <cardsfan853@gmail.com>
2014-05-04 16:24:16 -0500

    starting deathgrip, fixed a few abilities

---
+++ Assets/Scripts/Effects/BounceBetween.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBetween : MonoBehaviour 
+{
+    public GameObject origin;
+    public GameObject target;
+
+    public int BounceAmount;
+
+    public float WaitTime;
+    
+	void Awake () 
+    {
+	}
+	
+	void Update () 
+    {
+        StartCoroutine("bounce");
+	}
+
+    IEnumerable bounce()
+    {
+        for (int i = 0; i < BounceAmount;i++ )
+        {
+            transform.position = target.transform.position;
+
+            yield return new WaitForSeconds(WaitTime);
+
+            transform.position = origin.transform.position;
+
+            yield return new WaitForSeconds(WaitTime);
+        }
+
+        yield return null;
+    }
+}
