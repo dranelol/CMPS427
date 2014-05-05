582c2a8abf21eae697fbd060a3a153c6e826930d
Davis725 <d.davis6835@gmail.com>
2014-05-05 17:22:03 -0500

    Enemy Health GUI
    Added a GUI Label that shows an enemies name and health when the mouse
    is over them. The health currently is shown as current/max

---
+++ Assets/Scripts/Entity/Enemy/MouseoverDisplay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseoverDisplay : MonoBehaviour {
+
+	public string name;
+	private bool show = false;
+	private bool mouseOver = false;
+	public float OffsetX = 10;
+	public float OffsetY = -10;
+
+	void OnMouseOver()
+	{
+		show = true;
+		mouseOver = true;
+	}
+
+	void OnMouseNotOver()
+	{
+		show = false;
+	}
+
+	void Update()
+	{
+		if (!mouseOver) OnMouseNotOver();
+		mouseOver = false;
+	}
+
+	void OnGUI()
+	{
+		if (show) 
+		{
+			float currentHP = GetComponent<Entity>().CurrentHP;
+			float maxHP = GetComponent<Entity> ().currentAtt.Health;
+
+			Rect temp = new Rect (Input.mousePosition.x + OffsetX, 
+			                      Screen.height - Input.mousePosition.y + OffsetY, 150, 40);
+
+			GUI.Label (temp, name + "\n" + currentHP + "/" + maxHP);
+		}
+	}
+}
