8871463f464027bc2a08b5c58945c12019c6d450
ryanadair009 <ryan.adair009@gmail.com>
2014-05-07 12:06:43 -0500

    broken shit

---
+++ Assets/Scripts/Effects/ParticleCleanup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// destroy particle systems once they stop emitting
+/// </summary>
+public class ParticleCleanup : MonoBehaviour 
+{
+	void Update () 
+    {
+        if (particleSystem.emissionRate == 0)
+        {
+            Destroy(gameObject, particleSystem.duration);
+        }
+	}
+}
