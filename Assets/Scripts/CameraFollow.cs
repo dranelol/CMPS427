fe6a9e1798bb5e621feb9afa54e32bc32d60aa46
dranelol <cardsfan853@gmail.com>
2014-06-12 22:42:57 -0500

    added minimap to game

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    public Transform Target;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        transform.position = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+	}
+}
